23043e2e24b802a331d54b180da6d16824563727
istine <imanchesebastian@gmail.com>
2023-09-15 10:46:08 +0100

    changed validation api implementation from class to object

--- src/ValidationAPI/CommonAuthFunctions.ts
+++ src/ValidationAPI/CommonAuthFunctions.ts
@@ -1,20 +1,16 @@
-export class CommonAuthFucntions {
-  static requiredField = (fieldValue: string): boolean => {
+export const CommonAuthFucntions = {
+  requiredField(fieldValue: string): boolean {
     if (!fieldValue) {
       return false;
     }
 
     return true;
-  };
+  },
 
-  static stringLengthvalid = (
-    fieldValue: string,
-    min: number,
-    max: number
-  ): boolean => {
+  stringLengthvalid(fieldValue: string, min: number, max: number): boolean {
     if (fieldValue.length >= min && fieldValue.length <= max) {
       return true;
     }
     return false;
-  };
-}
+  },
+};
